ccdca82b2b34457b8cdcce406615943595cc366c
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-01-30 23:23:06 +0200

    (Try the Pound character) Complete

---
+++ ex2.py
@@ -0,0 +1,12 @@
+# This print a "Hello" 
+print("Hello") 
+
+# Disable the Print code
+#print("This is a Disable print")
+
+# Enable the Print code and Comment on the end of the line
+print("This is a Enable print") # this print a "This is a Enable print"
+
+# Try inside the print # = (Pound character), and Comment on the end of the line
+print("hi pound char #") # is not Disable the Print code because is inside the string
+ 
